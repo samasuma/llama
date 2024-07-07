f91d0b244ae7d70d77e705e11958301384f0b9be
X <samuel.asuma@gmail.com>
2024-07-07 13:49:42 -0500

    clean up

--- starter.py
+++
@@ -1,8 +0,0 @@
-from llama_index.core import VectorStoreIndex, SimpleDirectoryReader
-
-documents = SimpleDirectoryReader("data").load_data()
-index = VectorStoreIndex.from_documents(documents)
-query_engine = index.as_query_engine()
-response = query_engine.query("What is the legal age to smoke weed?")
-
-print(type(response))
